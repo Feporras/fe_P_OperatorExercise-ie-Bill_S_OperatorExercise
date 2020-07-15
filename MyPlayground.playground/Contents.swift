fafa4a951be4b3973b1dfe19ce6ae5685c2d23a0
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-14 22:59:26 -0500

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,80 @@
+let nine = 9
+let ten = 10
+let eight = 8
+let seven = 7
+let six = 6
+let five = 5
+let four = 4
+let three = 3
+let two = 2
+let one = 1
+
+
+
+one + one
+one + two
+one + three
+one + four
+one + five
+one + six
+one + seven
+one + eight
+one + nine
+one + ten
+
+one * one
+one * two
+one * three
+one * four
+one * five
+one * six
+one * seven
+one * eight
+one * nine
+one * ten
+
+one / one
+one / two
+one / three
+one / four
+one / five
+one / six
+one / seven
+one / eight
+one / nine
+one / ten
+
+
+two + one
+two + two
+two + three
+two + four
+two + five
+two + six
+two + seven
+two + eight
+two + nine
+two + ten
+
+two * one
+two * two
+two * three
+two * four
+two * five
+two * six
+two * seven
+two * eight
+two * nine
+two * ten
+
+two / one
+two / two
+two / three
+two / four
+two / five
+two / six
+two / seven
+two / eight
+two / nine
+two / ten
+
